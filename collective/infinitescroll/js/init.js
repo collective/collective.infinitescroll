dc816d765802255296fa3e78884b86b7fef2e525
Johannes Raggam <raggam-nl@adm.at>
2013-02-19 16:07:07 +0100

    initialize

--- collective/infinitescroll/js/init.js
+++ collective/infinitescroll/js/init.js
@@ -1,8 +1,9 @@
 (function($) {
-
     $(document).ready(function() {
-        
-
+        $('#content-core').infinitescroll({
+            navSelector  : "div.listingBar",
+            nextSelector : "div.listingBar span.next a",
+            itemSelector : "#content-core"
+        });
     });
-
 })(jQuery);
