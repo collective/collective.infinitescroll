ef7efbadf8310202d5b83df82c7eca167ff7b89d
Johannes Raggam <raggam-nl@adm.at>
2013-02-20 15:19:21 +0100

    notes on b_size, fix item selector to generic #content-core

--- README.rst
+++ README.rst
@@ -10,11 +10,17 @@ view via ajax.
 It's an unintrusive javascript and doesn't alter any of the templates.
 
 .. note::
-    paulirish's infinite-scroll jQuery plugin parses the "next" button only on
-    initialization and calculates the urls to retrieve the contents by
-    incrementing an page counter.  Plone uses an item-offset instead of page
-    numbers. For Plone a more generic approach would be better: Parsing the
-    "next" buttons url on each ajax request.
+  paulirish's infinite-scroll jQuery plugin parses the "next" button only on
+  initialization and calculates the urls to retrieve the contents by
+  incrementing an page counter.  Plone uses an item-offset instead of page
+  numbers. For Plone a more generic approach would be better: Parsing the
+  "next" buttons url on each ajax request.
+
+.. note::
+  The batch size is fixed to 10 items. You may have to change the init script
+  and adapt it to your needs. TODO: automatically get the batch size or always
+  use the next button's href to get the url of the next page instead of relying
+  on a page counter.
 
 Author
 ======
--- collective/infinitescroll/js/init.js
+++ collective/infinitescroll/js/init.js
@@ -1,15 +1,16 @@
 (function($) {
 
     var batch_size = 10;
+    var next_button_selector = 'div.listingBar span.next a';
 
     function plone_path(next_page) {
-        path = $('div.listingBar span.next a').attr('href');
+        path = $(next_button_selector).attr('href');
         if (path.match(/^(.*?b_start.*?$)/)) {
             // Plone Batching
             parsed_path = path.match(/^(.*?b_start:int=)\d*(.*?$)/).slice(1);
-            return parsed_path[0] + (next_page-1)*batch_size + parsed_path[1] + '&ajax_load=1';
+            return parsed_path[0] + (next_page-1)*batch_size + parsed_path[1] + '&b_size='+ batch_size + '&ajax_load=1';
         }
-        return ['']
+        return [''];
     }
 
     $(document).ready(function() {
@@ -20,10 +21,11 @@
         $('#content-core').infinitescroll({
             //debug: true,
             navSelector:  "div.listingBar",
-            nextSelector: "div.listingBar span.next a",
-            itemSelector: "#content-core div dl",
+            nextSelector: next_button_selector,
+            itemSelector: "#content-core",
             path: plone_path,
             maxPage: max_page
         });
     });
+
 })(jQuery);
